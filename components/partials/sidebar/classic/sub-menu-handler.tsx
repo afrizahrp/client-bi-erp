a9ce867541603ce9ff23c595addf7fb412babc9c
afrizahrp <afriza.hrp@gmail.com>
2025-02-14 14:08:21 +0700

    update menu-font-size

--- components/partials/sidebar/classic/sub-menu-handler.tsx
+++ components/partials/sidebar/classic/sub-menu-handler.tsx
@@ -1,6 +1,6 @@
-"use client";
-import { Icon } from "@iconify/react";
-import { cn, translate } from "@/lib/utils";
+'use client';
+import { Icon } from '@iconify/react';
+import { cn, translate } from '@/lib/utils';
 
 const SubMenuHandler = ({
   item,
@@ -17,7 +17,7 @@ const SubMenuHandler = ({
   activeSubmenu: number | null;
   collapsed: boolean;
   hovered: boolean;
-  trans: any
+  trans: any;
 }) => {
   const { title } = item;
 
@@ -27,38 +27,38 @@ const SubMenuHandler = ({
         <div
           onClick={() => toggleSubmenu(index)}
           className={cn(
-            "flex  text-default-700 group font-medium text-sm capitalize px-[10px] py-3 rounded cursor-pointer transition-all duration-100 hover:bg-primary hover:text-primary-foreground group",
+            'flex  text-default-700 group font-small text-xs capitalize px-[10px] py-3 rounded cursor-pointer transition-all duration-100 hover:bg-primary hover:text-primary-foreground group',
             {
-              "bg-primary  text-primary-foreground": activeSubmenu === index,
+              'bg-primary  text-primary-foreground': activeSubmenu === index,
             }
           )}
         >
-          <div className="flex-1  gap-3 flex items-start">
-            <span className="inline-flex items-center     ">
-              <item.icon className="w-5 h-5" />
+          <div className='flex-1  gap-3 flex items-start'>
+            <span className='inline-flex items-center     '>
+              <item.icon className='w-5 h-5' />
             </span>
-            <div className=" ">{translate(title, trans)}</div>
+            <div className=' '>{translate(title, trans)}</div>
           </div>
-          <div className="flex-0">
+          <div className='flex-0'>
             <div
               className={cn(
-                " text-base rounded-full flex justify-center items-center transition-all duration-300 group-hover:text-primary-foreground",
+                ' text-base rounded-full flex justify-center items-center transition-all duration-300 group-hover:text-primary-foreground',
                 {
-                  "rotate-90  ": activeSubmenu === index,
-                  " text-default-500  ": activeSubmenu !== index,
+                  'rotate-90  ': activeSubmenu === index,
+                  ' text-default-500  ': activeSubmenu !== index,
                 }
               )}
             >
               <Icon
-                icon="heroicons:chevron-right-20-solid"
-                className="h-5 w-5"
+                icon='heroicons:chevron-right-20-solid'
+                className='h-5 w-5'
               />
             </div>
           </div>
         </div>
       ) : (
-        <div className="inline-flex cursor-pointer items-center justify-center data-[state=open]:bg-primary-100 data-[state=open]:text-primary  w-12 h-12  rounded-md">
-          <item.icon className="w-6 h-6" />
+        <div className='inline-flex cursor-pointer items-center justify-center data-[state=open]:bg-primary-100 data-[state=open]:text-primary  w-12 h-12  rounded-md'>
+          <item.icon className='w-6 h-6' />
         </div>
       )}
     </>
--- components/partials/sidebar/module/index.tsx
+++ components/partials/sidebar/module/index.tsx
@@ -1,21 +1,21 @@
-import React, { useEffect, useState } from "react";
-import { cn, isLocationMatch, getDynamicPath, translate } from "@/lib/utils";
-import { menusConfig, ModernNavType, } from "@/config/menus";
-import SingleIconMenu from "./single-icon-menu";
-import { useRouter, usePathname } from "next/navigation";
-import { useSidebar, useThemeStore } from "@/store";
-import MenuItem from "./menu-item";
-import NestedMenus from "./nested-menus";
-import Image from "next/image";
-import Link from "next/link";
-import FooterMenu from "./footer-menu";
-import { SiteLogo } from "@/components/svg";
-import { ScrollArea } from "@/components/ui/scroll-area";
-import LogoutFooter from "./logout-footer";
-import { useMediaQuery } from "@/hooks/use-media-query";
-import MenuOverlayPortal from "./MenuOverlayPortal";
-import { ChevronLeft } from "lucide-react";
-import { Button } from "@/components/ui/button";
+import React, { useEffect, useState } from 'react';
+import { cn, isLocationMatch, getDynamicPath, translate } from '@/lib/utils';
+import { menusConfig } from '@/config/menus';
+import SingleIconMenu from './single-icon-menu';
+import { useRouter, usePathname } from 'next/navigation';
+import { useSidebar, useThemeStore } from '@/store';
+import MenuItem from './menu-item';
+import NestedMenus from './nested-menus';
+import Image from 'next/image';
+import Link from 'next/link';
+import FooterMenu from './footer-menu';
+import { SiteLogo } from '@/components/svg';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import LogoutFooter from './logout-footer';
+import { useMediaQuery } from '@/hooks/use-media-query';
+import MenuOverlayPortal from './MenuOverlayPortal';
+import { ChevronLeft } from 'lucide-react';
+import { Button } from '@/components/ui/button';
 
 const ModuleSidebar = ({ trans }: { trans: any }) => {
   const menus = menusConfig?.sidebarNav?.modern || [];
@@ -28,8 +28,8 @@ const ModuleSidebar = ({ trans }: { trans: any }) => {
   const [multiNestedIndex, setMultiNestedIndex] = useState<number | null>(null);
   // mobile menu overlay
   const [menuOverlay, setMenuOverlay] = useState<boolean>(false);
-  const isDesktop = useMediaQuery("(min-width: 1280px)");
-  const isMobile = useMediaQuery("(min-width: 768px)");
+  const isDesktop = useMediaQuery('(min-width: 1280px)');
+  const isMobile = useMediaQuery('(min-width: 768px)');
 
   // location
 
@@ -81,7 +81,11 @@ const ModuleSidebar = ({ trans }: { trans: any }) => {
     setSubmenu(false);
     setCollapsed(false);
   }
-  function setActiveNestedMenu(menuIndex: number, nestedMenuIndex: number, childMenu: any) {
+  function setActiveNestedMenu(
+    menuIndex: number,
+    nestedMenuIndex: number,
+    childMenu: any
+  ) {
     setActiveIndex(menuIndex);
     setNestedIndex(nestedMenuIndex);
     setCurrentSubMenu(childMenu);
@@ -93,7 +97,7 @@ const ModuleSidebar = ({ trans }: { trans: any }) => {
     if (activeIndex !== null) {
       return menus[activeIndex].title;
     }
-    return "";
+    return '';
   };
 
   useEffect(() => {
@@ -134,7 +138,6 @@ const ModuleSidebar = ({ trans }: { trans: any }) => {
           });
         }
       });
-
     });
     if (!isMenuMatched) {
       setSubmenu(false);
@@ -146,29 +149,29 @@ const ModuleSidebar = ({ trans }: { trans: any }) => {
 
   return (
     <>
-      <div className="main-sidebar  pointer-events-none fixed start-0 top-0 z-[60] flex h-full xl:z-10 print:hidden">
+      <div className='main-sidebar  pointer-events-none fixed start-0 top-0 z-[60] flex h-full xl:z-10 print:hidden'>
         <div
           className={cn(
-            "border-default-200  dark:border-default-300 pointer-events-auto relative z-20 flex h-full w-[72px] flex-col border-r border-dashed   bg-card transition-all duration-300",
+            'border-default-200  dark:border-default-300 pointer-events-auto relative z-20 flex h-full w-[72px] flex-col border-r border-dashed   bg-card transition-all duration-300',
             {
-              "ltr:-translate-x-full rtl:translate-x-full ltr:xl:translate-x-0  rtl:xl:translate-x-0":
+              'ltr:-translate-x-full rtl:translate-x-full ltr:xl:translate-x-0  rtl:xl:translate-x-0':
                 !collapsed && subMenu,
-              "translate-x-0": collapsed,
+              'translate-x-0': collapsed,
             }
           )}
         >
-          <div className=" pt-4 ">
-            <Link href="/dashboard">
-              <SiteLogo className=" mx-auto text-primary h-8 w-8" />
+          <div className=' pt-4 '>
+            <Link href='/dashboard'>
+              <SiteLogo className=' mx-auto text-primary h-8 w-8' />
             </Link>
           </div>
           {/* end logo */}
-          <ScrollArea className=" pt-6 grow ">
+          <ScrollArea className=' pt-6 grow '>
             {menus.map((item, i) => (
               <div
                 key={i}
                 onClick={() => toggleSubMenu(i)}
-                className=" mb-3 last:mb-0"
+                className=' mb-3 last:mb-0'
               >
                 <SingleIconMenu
                   index={i}
@@ -186,41 +189,41 @@ const ModuleSidebar = ({ trans }: { trans: any }) => {
 
         <div
           className={cn(
-            "border-default-200 pointer-events-auto relative z-10 flex flex-col h-full w-[228px] border-r  bg-card   transition-all duration-300",
+            'border-default-200 pointer-events-auto relative z-10 flex flex-col h-full w-[228px] border-r  bg-card   transition-all duration-300',
             {
-              "rtl:translate-x-[calc(100%_+_72px)] translate-x-[calc(-100%_-_72px)]":
+              'rtl:translate-x-[calc(100%_+_72px)] translate-x-[calc(-100%_-_72px)]':
                 collapsed || subMenu,
             }
           )}
         >
-          {sidebarBg !== "none" && (
+          {sidebarBg !== 'none' && (
             <div
-              className=" absolute left-0 top-0   z-[-1] w-full h-full bg-cover bg-center opacity-[0.07]"
+              className=' absolute left-0 top-0   z-[-1] w-full h-full bg-cover bg-center opacity-[0.07]'
               style={{ backgroundImage: `url(${sidebarBg})` }}
             ></div>
           )}
-          <h2 className="text-lg  bg-transparent   z-50   font-semibold  flex gap-4 items-center   text-default-700 sticky top-0 py-4  px-4   capitalize ">
-            <span className=" block ">{translate(getMenuTitle(), trans)}</span>
+          <h2 className='text-lg  bg-transparent   z-50   font-semibold  flex gap-4 items-center   text-default-700 sticky top-0 py-4  px-4   capitalize '>
+            <span className=' block '>{translate(getMenuTitle(), trans)}</span>
             {!isDesktop && (
               <Button
-                size="icon"
-                variant="ghost"
+                size='icon'
+                variant='ghost'
                 onClick={() => {
                   setCollapsed(true);
                   setSubmenu(true);
                   setMenuOverlay(false);
                 }}
-                className="rounded-full h-8 w-8"
+                className='rounded-full h-8 w-8'
               >
-                <ChevronLeft className="w-5 h-5  " />
+                <ChevronLeft className='w-5 h-5  ' />
               </Button>
             )}
           </h2>
-          <ScrollArea className="h-[calc(100%-40px)]  grow ">
-            <div className="px-4 " dir={isRtl ? "rtl" : "ltr"}>
+          <ScrollArea className='h-[calc(100%-40px)]  grow '>
+            <div className='px-4 ' dir={isRtl ? 'rtl' : 'ltr'}>
               <ul>
                 {currentSubMenu?.map((childItem, j) => (
-                  <li key={j} className="mb-1.5 last:mb-0">
+                  <li key={j} className='mb-1.5 last:mb-0'>
                     <MenuItem
                       trans={trans}
                       childItem={childItem}
