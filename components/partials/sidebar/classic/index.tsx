48f3ad71a025c4c6469933e667c63380fa799b25
afrizahrp <afriza.hrp@gmail.com>
2025-02-14 14:49:51 +0700

    remove menlabel in sidebar classic

--- components/partials/sidebar/classic/index.tsx
+++ components/partials/sidebar/classic/index.tsx
@@ -1,17 +1,17 @@
-"use client";
-import React, { useState } from "react";
-import { cn, isLocationMatch, getDynamicPath } from "@/lib/utils";
-import { useSidebar, useThemeStore } from "@/store";
-import SidebarLogo from "../common/logo";
-import { menusConfig } from "@/config/menus";
-import MenuLabel from "../common/menu-label";
-import { Separator } from "@/components/ui/separator";
-import { ScrollArea } from "@/components/ui/scroll-area";
-import { usePathname } from "next/navigation";
-import SingleMenuItem from "./single-menu-item";
-import SubMenuHandler from "./sub-menu-handler";
-import NestedSubMenu from "../common/nested-menus";
-import AddBlock from "../common/add-block";
+'use client';
+import React, { useState } from 'react';
+import { cn, isLocationMatch, getDynamicPath } from '@/lib/utils';
+import { useSidebar, useThemeStore } from '@/store';
+import SidebarLogo from '../common/logo';
+import { menusConfig } from '@/config/menus';
+import MenuLabel from '../common/menu-label';
+import { Separator } from '@/components/ui/separator';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { usePathname } from 'next/navigation';
+import SingleMenuItem from './single-menu-item';
+import SubMenuHandler from './sub-menu-handler';
+import NestedSubMenu from '../common/nested-menus';
+import AddBlock from '../common/add-block';
 const ClassicSidebar = ({ trans }: { trans: string }) => {
   const { sidebarBg } = useSidebar();
   const [activeSubmenu, setActiveSubmenu] = useState<number | null>(null);
@@ -69,17 +69,17 @@ const ClassicSidebar = ({ trans }: { trans: string }) => {
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
       className={cn(
-        "fixed  z-[999] top-0  bg-card h-full hover:!w-[248px]  border-r  ",
+        'fixed  z-[999] top-0  bg-card h-full hover:!w-[248px]  border-r  ',
         {
-          "w-[248px]": !collapsed,
-          "w-[72px]": collapsed,
-          "shadow-md": collapsed || hovered,
+          'w-[248px]': !collapsed,
+          'w-[72px]': collapsed,
+          'shadow-md': collapsed || hovered,
         }
       )}
     >
-      {sidebarBg !== "none" && (
+      {sidebarBg !== 'none' && (
         <div
-          className=" absolute left-0 top-0   z-[-1] w-full h-full bg-cover bg-center opacity-[0.07]"
+          className=' absolute left-0 top-0   z-[-1] w-full h-full bg-cover bg-center opacity-[0.07]'
           style={{ backgroundImage: `url(${sidebarBg})` }}
         ></div>
       )}
@@ -87,15 +87,15 @@ const ClassicSidebar = ({ trans }: { trans: string }) => {
       <SidebarLogo hovered={hovered} />
 
       <ScrollArea
-        className={cn("sidebar-menu  h-[calc(100%-80px)] ", {
-          "px-4": !collapsed || hovered,
+        className={cn('sidebar-menu  h-[calc(100%-80px)] ', {
+          'px-4': !collapsed || hovered,
         })}
       >
         <ul
-          dir={isRtl ? "rtl" : "ltr"}
-          className={cn(" space-y-1", {
-            " space-y-2 text-center": collapsed,
-            "text-start": collapsed && hovered,
+          dir={isRtl ? 'rtl' : 'ltr'}
+          className={cn(' space-y-1', {
+            ' space-y-2 text-center': collapsed,
+            'text-start': collapsed && hovered,
           })}
         >
           {menus.map((item, i) => (
@@ -112,9 +112,9 @@ const ClassicSidebar = ({ trans }: { trans: string }) => {
               )}
 
               {/* menu label */}
-              {item.isHeader && !item.child && (!collapsed || hovered) && (
+              {/* {item.isHeader && !item.child && (!collapsed || hovered) && (
                 <MenuLabel item={item} trans={trans} />
-              )}
+              )} */}
 
               {/* sub menu */}
               {item.child && (
@@ -136,8 +136,6 @@ const ClassicSidebar = ({ trans }: { trans: string }) => {
                       activeSubmenu={activeSubmenu}
                       item={item}
                       index={i}
-
-
                       trans={trans}
                     />
                   )}
