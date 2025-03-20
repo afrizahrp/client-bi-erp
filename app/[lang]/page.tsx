d95495c7b18af71e91d1624dcf2ca4d2097802c6
AFRIZAHRP <afriza.hrp@gmail.com>
2025-03-20 21:32:08 +0700

    update-sidebar

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import LoginForm from '@/app/[lang]/auth/(login)/login/login-form';
 
-// const page = () => {
-//   return <LandingPage />;
-// };
-
-// export default page;
-
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800'>
